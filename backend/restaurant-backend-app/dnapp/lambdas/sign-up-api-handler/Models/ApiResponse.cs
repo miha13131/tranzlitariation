0eb8bad46b4e16ca90931f8580b699d63e1a3d3a
miha13131 <87076262+miha13131@users.noreply.github.com>
2026-03-02 00:22:29 +0100

    Delete backend directory

--- backend/restaurant-backend-app/dnapp/lambdas/sign-up-api-handler/Models/ApiResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace Function.Models;
-
-public class ApiResponse<T>
-{
-    public string Status { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public T? Value { get; set; }
-}
